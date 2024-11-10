451fa5e22a5a8eab1986cae2a35b528b3096f371
Collins Boakye <colly0087@gmail.com>
2024-11-10 05:46:04 -0800

    Refactoring of Prop Drill to Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import UserContext from './components/UserContext'
 import ProfilePage from './components/ProfilePage'
+import UserProfile from './components/UserProfile';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
@@ -8,6 +9,7 @@ function App() {
   return (
     <UserContext.Provider value={userData}>
       <ProfilePage />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </UserContext.Provider>
   );
 }
