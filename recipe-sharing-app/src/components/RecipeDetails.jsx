54cddc5240bbba19ba472295cda411923b32f049
Collins Boakye <colly0087@gmail.com>
2024-11-17 13:48:21 -0800

    Zustand store with recipe application

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import useRecipeStore from "../store/recipeStore";
+import useRecipeStore from "./recipeStore";
 import EditRecipeForm from "./EditRecipeForm";
 import DeleteRecipeButton from "./DeleteRecipeButton";
 import { useParams } from "react-router-dom";
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import useRecipeStore from "../store/recipeStore";
+import useRecipeStore from "./recipeStore";
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import SearchBar from "./SearchBar";
