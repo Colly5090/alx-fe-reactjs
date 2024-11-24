aed405cd61e9d0eb78fa2a1d6d42950f5c406b86
Collins Boakye <colly0087@gmail.com>
2024-11-24 14:06:42 -0800

    caches with jquery

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useQuery } from 'react-query'
+
+//Function to handle data fetching
+const fetchData = async () => {
+    console.log("Fetching data from API...");
+    const apiData = await fetch('https://jsonplaceholder.typicode.com/posts')
+    return apiData.json();
+}
+
+// PostsComponent: Uses useQuery to manage fetching, loading, and error states
+//refetch Allows us to manually trigger a refetch with a button or a trigger
+
+    function PostsComponent() {
+    const { data, error, isLoading, refetch } = useQuery('mycache', fetchData, {staleTime: Infinity,});
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    if (error) {
+        return <div>Error loading data</div>
+    }
+  return (
+    <div>
+        <button type='button' onClick={() => refetch()} disabled={isLoading}>Refresh Data</button>
+        {isLoading && <div>Refreshing...</div>}
+        {data.map(item => (
+            <div key={item.id}>
+                <h2>{item.title}</h2>
+                <p>{item.body}</p>
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default PostsComponent
