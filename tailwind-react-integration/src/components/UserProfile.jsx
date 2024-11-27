f93604c50781ddd361bd6423226fc46b455874c1
Collins Boakye <colly0087@gmail.com>
2024-11-26 19:11:11 -0800

    Tailwind utility application

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="user-profile">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs
+    hover:shadow-xl">
       <img src="https://via.placeholder.com/150" alt="User" 
       className="rounded-full w-36 h-36 mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110
       transition-transform duration-300 ease-in-out"/>
